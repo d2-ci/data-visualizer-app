1048651e52e8eea6d3c28dbe152e38442146e1b3
GitHub <noreply@github.com>
2020-01-03 15:20:15 +0000

    a52f7f3 fix: upgrade cli-app-scripts to fix package resolution (#540)

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.71457362da1da210c2c6cd8432868b1a.js"
+  "./precache-manifest.1ebc83fe6dce0ff5517342554f132bb6.js"
 );
 
 self.addEventListener('message', (event) => {
