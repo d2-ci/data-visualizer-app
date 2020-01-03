334b459ea2fc30a11310d2c9309b249aff98a080
GitHub <noreply@github.com>
2020-01-03 13:43:37 +0000

    14f6f70 refactor: use visualization API endpoint (#527)

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.2ffa9145fc74e51336e9407c019d9771.js"
+  "./precache-manifest.71457362da1da210c2c6cd8432868b1a.js"
 );
 
 self.addEventListener('message', (event) => {
