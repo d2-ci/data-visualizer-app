453705707b4fd208c712fb3f8338e9b1f6ec11a2
GitHub <noreply@github.com>
2020-01-20 10:49:18 +0000

    ef16c68 fix: provide useful error messages (DHIS2-5029) (#552)

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.415bcd6cd7d1ad045575240d75991359.js"
+  "./precache-manifest.58cfca087f19cbd0c94bffeb13fbdc5f.js"
 );
 
 self.addEventListener('message', (event) => {
