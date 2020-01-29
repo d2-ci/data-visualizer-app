6a9ce03fd712f6593b938d7349011774514d5482
Austin McGee <947888+amcgee@users.noreply.github.com>
2020-01-29 11:46:47 +0000

    b14e278 fix: correct option name

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.1691fc92757d5a2fc64af7960faf23b8.js"
+  "./precache-manifest.7b27909fd83a55482f6136ee3fa39f22.js"
 );
 
 self.addEventListener('message', (event) => {
