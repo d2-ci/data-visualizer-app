f64981e7d41c93fc24a99a154c992e047f8e1bc0
GitHub <noreply@github.com>
2020-02-06 12:57:26 +0000

    97c53c8 Merge branch 'master' into fix/axis-names

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.2fd7288eec2558115732b5e234644546.js"
+  "./precache-manifest.5c4b8cf71a509228492d8106e1bf78b8.js"
 );
 
 self.addEventListener('message', (event) => {
