92b32eb8c8af47d63a19b045277f7ab3370ebc32
GitHub <noreply@github.com>
2020-02-10 08:47:12 +0000

    790c1ae Merge branch 'master' into fix/cast-numeric-options

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.595fdda270561a125f125a9ffcc41dd1.js"
+  "./precache-manifest.978d1c314e45d43411c39616d5ff0eb1.js"
 );
 
 self.addEventListener('message', (event) => {
