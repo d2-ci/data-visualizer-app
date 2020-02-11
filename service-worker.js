d29b1dd161622e2e7e9793c9862e4dba9e3b72e8
GitHub <noreply@github.com>
2020-02-11 14:50:00 +0000

    c943515 Merge branch 'master' into fix/empty-dim-chips

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.8df501fb51ad1245203db20eed064661.js"
+  "./precache-manifest.78f856e02b5693ab6d47d80b9a0cc04b.js"
 );
 
 self.addEventListener('message', (event) => {
