55b128990a1b45c23ae935cd113184ef246c2e0b
GitHub <noreply@github.com>
2020-02-14 14:51:43 +0000

    d8e8e9e fix: minor ui glitches (#667)

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.6023bab9eda448d1fbb1278d90160b60.js"
+  "./precache-manifest.5508972c82df1cf4b44df78904e3298e.js"
 );
 
 self.addEventListener('message', (event) => {
