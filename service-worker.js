12f011385b6526b1b9e0467cc85445fc3bcb2676
Martin Ohlson <martin@moid.se>
2020-02-14 14:19:16 +0000

    d6a3dbc chore: regenerated pot file

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.fe1d5c141048fcc91b1e9a44d37b65bd.js"
+  "./precache-manifest.5508972c82df1cf4b44df78904e3298e.js"
 );
 
 self.addEventListener('message', (event) => {
