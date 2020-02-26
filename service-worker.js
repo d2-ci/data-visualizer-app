89ce2cbef673560135ba681c38d8a3d1c1dc1bbc
GitHub <noreply@github.com>
2020-02-26 12:40:34 +0000

    81a1bcd chore(deps-dev): bump loglevel from 1.6.6 to 1.6.7

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.e9c69693ab0bdec5f9f66cc5e760f466.js"
+  "./precache-manifest.93b30ae3a885d35ecc5e521b4a155611.js"
 );
 
 self.addEventListener('message', (event) => {
