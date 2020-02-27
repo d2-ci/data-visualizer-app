cc731ffb31c87fc19e07b61cd1c3060a6c97bc93
GitHub <noreply@github.com>
2020-02-27 11:13:12 +0000

    b26bb27 fix: remove data dimension validation (#738)

--- precache-manifest.3f332198586ce2f050e9a28190871adc.js
+++ precache-manifest.3f332198586ce2f050e9a28190871adc.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "384284c5a4763c081640c2e5f9c4f5b3",
+    "revision": "4730f64dd44d6234ca7befa9379ca384",
     "url": "./index.html"
   },
   {
@@ -12,7 +12,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/css/119.e4705d68.chunk.css"
   },
   {
-    "revision": "1c2968de7cfa1194b921",
+    "revision": "e74ebf8493e3903e6409",
     "url": "./static/css/app.3b702fc8.chunk.css"
   },
   {
@@ -492,16 +492,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/js/99.24af83ab.chunk.js"
   },
   {
-    "revision": "1c2968de7cfa1194b921",
-    "url": "./static/js/app.6353a90f.chunk.js"
+    "revision": "e74ebf8493e3903e6409",
+    "url": "./static/js/app.b6af63c8.chunk.js"
   },
   {
     "revision": "b53556d81a0aca037cd4",
     "url": "./static/js/main.5be6b149.chunk.js"
   },
   {
-    "revision": "4f7ada06e59611f92eac",
-    "url": "./static/js/runtime-main.ff65d811.js"
+    "revision": "dbf2b8f07c0d58257521",
+    "url": "./static/js/runtime-main.2b290a3f.js"
   },
   {
     "revision": "5cb7edfceb233100075dc9a1e12e8da3",
