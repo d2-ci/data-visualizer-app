76cbbdc8eddc8a94b124a3a0e4518d389a7a0577
GitHub <noreply@github.com>
2020-03-03 08:43:23 +0000

    a42e716 chore(deps-dev): bump redux-mock-store from 1.5.3 to 1.5.4

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.6a1937dc7286a81906b889f3535956f8.js"
+  "./precache-manifest.812e7dd495ed100dcf7b2b83a48261a3.js"
 );
 
 self.addEventListener('message', (event) => {
