6890106bbe73ec8dded12fca2daad768da6bcd4d
GitHub <noreply@github.com>
2020-03-04 17:50:34 +0000

    919d7ae chore(deps-dev): bump enzyme from 3.10.0 to 3.11.0

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.afd2e52036d9066be7a6578cd4a0c2fa.js"
+  "./precache-manifest.16139ec5320a3be3823dfdb3947337f2.js"
 );
 
 self.addEventListener('message', (event) => {
