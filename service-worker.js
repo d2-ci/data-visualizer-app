b17812f133117b72b9471eb78d62087ec8ff6957
Jan Henrik Overland <janhenrik.overland@gmail.com>
2020-03-10 22:08:57 +0000

    c6ed440 fix: upgrade analytics 4.3.11 -> 4.3.15

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.6fc9372e6d598ee337fb61434014deaf.js"
+  "./precache-manifest.f98dcd43e61c9cd43bfc9f19e6fab17e.js"
 );
 
 self.addEventListener('message', (event) => {
