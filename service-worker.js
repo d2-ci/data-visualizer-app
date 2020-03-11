7733032a94e51cbc48dbb6fc95158207d95f6815
GitHub <noreply@github.com>
2020-03-11 02:11:05 +0000

    4cd9caf fix: upgrade d2 to latest (#794)

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.f98dcd43e61c9cd43bfc9f19e6fab17e.js"
+  "./precache-manifest.b487a387bff2e5b2fa4d39c8cde62187.js"
 );
 
 self.addEventListener('message', (event) => {
