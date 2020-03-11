7c693832990aaae8bb27266942c80aab8b77ce7e
Austin McGee <947888+amcgee@users.noreply.github.com>
2020-03-11 03:50:07 +0000

    678a6fd chore: update to 4.3.17

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.66a7905828ff9205a4ee2cdba6485893.js"
+  "./precache-manifest.218a04cbc366f354a1e5a6dbbc3974f8.js"
 );
 
 self.addEventListener('message', (event) => {
