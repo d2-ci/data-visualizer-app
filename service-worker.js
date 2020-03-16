7094a9f3b586b27d143f97479e5b255da48b6e76
GitHub <noreply@github.com>
2020-03-16 11:17:20 +0000

    38891db chore(deps): bump @material-ui/core from 3.9.3 to 4.9.5

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.585d339882acde2f6514e35f822ea517.js"
+  "./precache-manifest.e0327c335ab6c7941a031fe91ea6bf7f.js"
 );
 
 self.addEventListener('message', (event) => {
