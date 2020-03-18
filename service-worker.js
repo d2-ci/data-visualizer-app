ce975413b7e090c8dbc0e7c336c0847e5b2da8a7
Jan Henrik Overland <janhenrik.overland@gmail.com>
2020-03-18 15:59:33 +0000

    cfb6e71 Merge remote-tracking branch 'origin/master' into v34

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.218a04cbc366f354a1e5a6dbbc3974f8.js"
+  "./precache-manifest.810a9d804acef2b2cee41b5c68ad43c3.js"
 );
 
 self.addEventListener('message', (event) => {
