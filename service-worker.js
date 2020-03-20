4d7788d6573b70f250d074528c9c35d7731cab35
GitHub <noreply@github.com>
2020-03-20 08:55:13 +0000

    1a4955d fix: upgrade analytics for chart title fix [DHIS2-8473] (#807)

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.810a9d804acef2b2cee41b5c68ad43c3.js"
+  "./precache-manifest.59832dfaf8c9117ce9d6ede038c3ede9.js"
 );
 
 self.addEventListener('message', (event) => {
