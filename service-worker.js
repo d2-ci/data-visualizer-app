6bf9e276d14d3378aea9d99f9672f2016c27e8ce
Edoardo Sabadelli <edoardo@dhis2.org>
2020-03-20 13:13:47 +0000

    1aaf207 refactor: simplify code and cleanup

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.246ae644e2eeafedbe32436a59fed981.js"
+  "./precache-manifest.846b35f2a91e802f136ad3d93edaadbc.js"
 );
 
 self.addEventListener('message', (event) => {
