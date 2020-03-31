30cf53f6a8be9088883ba46adf0eddd6c9e25816
Jan Henrik Overland <janhenrik.overland@gmail.com>
2020-03-31 21:19:43 +0000

    2fd3f4c fix: analytics@4.3.25 --exact

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.945946641de731c6f8838672f97e9a3e.js"
+  "./precache-manifest.9ba57cd3cbf263dadd6cb5ff695170a4.js"
 );
 
 self.addEventListener('message', (event) => {
