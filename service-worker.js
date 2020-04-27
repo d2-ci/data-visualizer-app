eea19e8cba2c89bca8a1202c25b4c511568c4e1e
GitHub <noreply@github.com>
2020-04-27 09:37:46 +0000

    b9c9123 chore(deps): bump redux from 4.0.4 to 4.0.5

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.b242cc4a77746f7f50631aca44f46381.js"
+  "./precache-manifest.0953c3d72c15dd241657ba4366cb96f9.js"
 );
 
 self.addEventListener('message', (event) => {
