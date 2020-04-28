4cd7e5c182807712f069c3ceec72dc8f99714c9b
GitHub <noreply@github.com>
2020-04-28 06:53:32 +0000

    b21a23e Merge branch 'master' into refactor/mui-style

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.a67c9df3d64ce55c34053e2add5127f8.js"
+  "./precache-manifest.d5c14c132ae232f347ba1ad3c87df5d9.js"
 );
 
 self.addEventListener('message', (event) => {
