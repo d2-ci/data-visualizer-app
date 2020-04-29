2c69854017e769ba478211577c2eaa38c083e9bd
GitHub <noreply@github.com>
2020-04-29 12:28:50 +0000

    32569fb Merge branch 'master' into fix/tooltip-limit

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.660c5ecdd4be87989a8fdf3fc48d0deb.js"
+  "./precache-manifest.d1b2d683023c5f050a934f44b3d2ae08.js"
 );
 
 self.addEventListener('message', (event) => {
