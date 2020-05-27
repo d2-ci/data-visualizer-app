aae3ae0170dd43473298278386d8f0d8eacdfcd7
GitHub <noreply@github.com>
2020-05-27 10:39:49 +0000

    1630625 chore(deps-dev): bump cypress from 3.6.1 to 4.7.0

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.46b8c5d89b5940717f9d1043da8a0f4d.js"
+  "./precache-manifest.3263f099edceb62cd271dabcfb4220b0.js"
 );
 
 self.addEventListener('message', (event) => {
