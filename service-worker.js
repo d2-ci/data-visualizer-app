3e1eb41f108ea0efa1d455d6986e14cece838d2a
GitHub <noreply@github.com>
2020-06-23 18:18:07 +0000

    d705c41 chore(deps): bump react-redux from 5.1.1 to 7.2.0

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.d720190c5c575744fd92f45d6b9ba5ad.js"
+  "./precache-manifest.a25cc853f00f1e698157e699834bda53.js"
 );
 
 self.addEventListener('message', (event) => {
