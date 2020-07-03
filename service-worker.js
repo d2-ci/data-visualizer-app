01ab29b282b2b62be1a9a7dd15f51c4552d3b442
Martin Ohlson <martin@moid.se>
2020-07-03 13:10:14 +0000

    43324b9 fix: zindex for line types

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.69323b437423c31f17795d20529ab496.js"
+  "./precache-manifest.64bd2c47d1f2ac4033005662c030a94a.js"
 );
 
 self.addEventListener('message', (event) => {
