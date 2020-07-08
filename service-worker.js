131c9348a2f98ec4279754b2a58507562fe45c9d
Martin Ohlson <martin@moid.se>
2020-07-08 20:22:30 +0000

    b2bc385 test: fixed failing test

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.20527f68668e52e9c3b9e8b123816144.js"
+  "./precache-manifest.a55cefd3f9e0d7cb24ebcd200d3d55d5.js"
 );
 
 self.addEventListener('message', (event) => {
