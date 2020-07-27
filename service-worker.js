937e551026f5af32cf6f35c72d4f644fbbf442a4
GitHub <noreply@github.com>
2020-07-27 10:50:43 +0000

    8e0d52b chore(deps-dev): bump @dhis2/cli-style from 5.0.2 to 7.0.0

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.2ae42d4b358481bb7ac6f2475a5f3136.js"
+  "./precache-manifest.1c06316ce4eeb1b27c1b95215df99dfc.js"
 );
 
 self.addEventListener('message', (event) => {
