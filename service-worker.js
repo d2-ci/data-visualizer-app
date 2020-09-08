ad1d5fb649c70766a03456f96df9527d54771748
Jan Henrik Overland <janhenrik.overland@gmail.com>
2020-09-08 16:35:40 +0000

    bcd6af3 chore: update analytics dep

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.6f157850ad4e9896adeb210dec41cb46.js"
+  "./precache-manifest.87de9be8b8baf5fa92ba45447cd789cd.js"
 );
 
 self.addEventListener('message', (event) => {
