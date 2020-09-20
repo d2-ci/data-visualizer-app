bed52e7c51ce5d13c6e40dda304f7a5b72d1f86e
GitHub <noreply@github.com>
2020-09-20 21:02:11 +0000

    a890541 Merge branch 'master' into fix/legend-option-radio-buttons-size

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.1df6c2aff204be2860e37e016e6c29cd.js"
+  "./precache-manifest.8f6a4a7ed1a6a83f9dcf703902b0e022.js"
 );
 
 self.addEventListener('message', (event) => {
