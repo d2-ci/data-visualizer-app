b5b7a3b9318141323b0e410e897fd89c00b262fe
GitHub <noreply@github.com>
2020-09-25 11:54:53 +0000

    d59be41 Merge branch '34.x' into backport/rich-text-fix-DHIS2-9382-34.x

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.a11435a6803f5cbefee7b0ff0f9d7d60.js"
+  "./precache-manifest.f0536c0f3f6bdfc060503af03b71ccd9.js"
 );
 
 self.addEventListener('message', (event) => {
