aec34f84ba8f7c554a8e51fec46cc44f020d687c
GitHub <noreply@github.com>
2020-11-19 09:44:05 +0000

    6fec7a6 chore(deps): bump @dhis2/app-runtime from 2.3.0 to 2.6.1

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.0d7baf0c5d725aad1b3447ca95408cba.js"
+  "./precache-manifest.1a8b8a1c5bbfc7dc2d7e5a8313a33e3d.js"
 );
 
 self.addEventListener('message', (event) => {
