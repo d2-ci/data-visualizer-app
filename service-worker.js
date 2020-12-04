792dba8afc4942afadfbc9154d46e842b9e9a925
GitHub <noreply@github.com>
2020-12-04 10:03:41 +0000

    a962cb8 chore(deps-dev): bump @dhis2/cypress-commands from 3.1.2 to 4.0.0

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.1eb611a578009d209824da77184cdbe9.js"
+  "./precache-manifest.742c5ee1edadd6326f4934856c1c4d9d.js"
 );
 
 self.addEventListener('message', (event) => {
