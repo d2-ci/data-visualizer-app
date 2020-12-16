6948cfa204436b7bed90fc8552246c43d39333e3
Edoardo Sabadelli <edoardo@dhis2.org>
2020-12-16 13:26:51 +0000

    72cd39e test: revert changes and disable eslint rules instead

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.8fda7e01125d99d59162fe7a52042629.js"
+  "./precache-manifest.6b2e3121b8d997825646a21eb4df47ed.js"
 );
 
 self.addEventListener('message', (event) => {
