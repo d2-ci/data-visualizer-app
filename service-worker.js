1c8242e07f35bb97643a4423969595d0026268d3
Martin Ohlson <martin@moid.se>
2021-03-30 08:16:06 +0000

    52577ac fix: bump version in plugin package.json

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.d8157504751c87db1d12de1f2734083f.js"
+  "./precache-manifest.6bbc58529b426929f693402b89b49c1c.js"
 );
 
 self.addEventListener('message', (event) => {
